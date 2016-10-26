a8338d09ca41530727816a53b43170e6a0fff65d
Cristian Cristea <cristiancristea.1987@gmail.com>
2016-10-26 08:10:48 +0200

    Add navbar animation on scroll

--- js/main.js
+++ js/main.js
@@ -2,35 +2,35 @@
 
 jQuery(document).ready(function($) {
   $(document).scroll(function(){
-  var lastPageYOffset = 0;
-  var $nav = $('nav');
-  var currentPageYOffset = $(this).scrollTop();
+    var lastPageYOffset = 0;
+    var $nav = $('nav');
+    var currentPageYOffset = $(this).scrollTop();
 
-  if (currentPageYOffset > lastPageYOffset ) {
-    $nav.css({
-      // 'padding-top': '1.3em',
-      'transform': 'translate3d(0, -30%, 0)',
-      'transition': '.4s'
-    }).find('.contact-nav').css({
-      'margin-top': '1.6em',
-      'transition': '.4s'
-    }).find('.contact-nav__link svg#Layer_1').css({
-      'width': '24px',
-      'transition': '.4s'
-    });
-  } else {
-    $nav.css({
-      // 'padding-top': '.625em',
-      'transform': 'translate3d(0, 0, 0)',
-      'transition': '.4s'
-    }).find('.contact-nav').css({
-      'margin-top': '1em',
-      'transition': '.4s'
-    }).find('.contact-nav__link svg#Layer_1').css({
-      'width': 'initial',
-      'transition': '.4s'
-    });
-  }
-});
+    if (currentPageYOffset > lastPageYOffset ) {
+      $nav.css({
+        // 'padding-top': '1.3em',
+        'transform': 'translate3d(0, -30%, 0)',
+        'transition': '.4s'
+      }).find('.contact-nav').css({
+        'margin-top': '1.6em',
+        'transition': '.4s'
+      }).find('.contact-nav__link svg#Layer_1').css({
+        'width': '24px',
+        'transition': '.4s'
+      });
+    } else {
+      $nav.css({
+        // 'padding-top': '.625em',
+        'transform': 'translate3d(0, 0, 0)',
+        'transition': '.4s'
+      }).find('.contact-nav').css({
+        'margin-top': '1em',
+        'transition': '.4s'
+      }).find('.contact-nav__link svg#Layer_1').css({
+        'width': 'initial',
+        'transition': '.4s'
+      });
+    }
+  });
 
 });
