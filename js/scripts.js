c3eb4831beec2a135a26e25eef82c104e0892e0e
Cristian Cristea <cristiancristea.1987@gmail.com>
2017-09-18 14:10:08 +0200

    Add hover effect on homepage

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 $(function() {
+  // toggle dropdown and close tag on mobile
   $("#nav-icon").click(function() {
     $(this)
       .toggleClass("open")
       .next("#dropdown")
       .slideToggle("500");
+  });
+
+  $("#projects")
+    .find("a")
+    .on("hover", function(e) {
+      console.log("hover");
+    });
+
+  // hover effect on project thumbnails
+  const $projects = $("#projects li");
+  $projects.hover(function(e) {
+    let $this = $(this);
+    $projects.not($this).toggleClass("blur");
   });
 });
